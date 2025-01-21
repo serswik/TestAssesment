68a86623ac2264d2d4aefeceefb7d7e7c31dcce4
serswik <megasergik@icloud.com>
2025-01-21 11:19:38 +0200

    Added TaxiTrip model

---
+++ Models/TaxiTrip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAssesment.Models
+{
+    public class TaxiTrip
+    {
+        public DateTime tpep_pickup_datetime { get; set; }
+        public DateTime tpep_dropoff_datetime { get; set; }
+        public int passenger_count { get; set; }
+        public double trip_distance { get; set; }
+        public string store_and_fwd_flag {  get; set; }
+        public int PULocationID { get; set; }
+        public int DOLocationID { get; set; }
+        public decimal fare_amount {  get; set; }
+        public decimal tip_amount { get; set; }
+    }
+}
